4366bca71c444a37033e0204b209a5d01e0401e1
Rourke Sekelsky <rourke.sekelsky@gmail.com>
2018-11-05 11:53:24 -0500

    Created delegated aiming component on tank to handle aim/fire

---
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankAimingComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	void AimAt(FVector HitLocation);
+
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+private:
+	// Sets default values for this component's properties
+	UTankAimingComponent();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UStaticMeshComponent* Barrel = nullptr;
+};
